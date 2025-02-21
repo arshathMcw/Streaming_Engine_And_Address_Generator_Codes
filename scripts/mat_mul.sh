854e3feb2771cb42e85af23973e20dec07951f8e
arshathahamed10 <arshathahamed392@gmail.com>
2025-02-21 10:24:39 +0530

    added gitignore

--- scripts/mat_mul.sh
+++
@@ -1,3 +0,0 @@
-# 1 clock all 512 bit
-g++ -I /home/arshathahamed10/ti/ti-cgt-c7000_4.1.0.LTS/host_emulation/include/C7100/ -o output ../matrix_multiplication.cpp -L /home/arshathahamed10/ti/ti-cgt-c7000_4.1.0.LTS/host_emulation/*.a 
-./output
--- scripts/rgb_to_grayscale.sh
+++
@@ -1,2 +0,0 @@
-g++ -I /home/arshathahamed10/ti/ti-cgt-c7000_4.1.0.LTS/host_emulation/include/C7100/ -o output ../rgb_to_grayscale.cpp `pkg-config --cflags --libs opencv4` -L /home/arshathahamed10/ti/ti-cgt-c7000_4.1.0.LTS/host_emulation/*.a 
-./output
